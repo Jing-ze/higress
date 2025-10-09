68b7ee9cd5ad9f9b829fa42fad41782eab522ce8
jingze <daijingze.djz@alibaba-inc.com>
2025-10-09 15:09:05 +0800

    test
    Change-Id: Id9abaad36ca8fa07268dbac68cc4d8e3b81450fb
    Co-developed-by: Cursor <noreply@cursor.com>

--- plugins/wasm-go/extensions/ai-image-reader/main.go
+++ plugins/wasm-go/extensions/ai-image-reader/main.go
@@ -119,7 +119,6 @@ func executeReadImage(imageUrls []string, config Config, query string, queryInde
 						log.Errorf("modify request message content failed, err:%v, body:%s", err, body)
 					} else {
 						log.Debugf("modified body:%s", modifiedBody)
-
 						proxywasm.ReplaceHttpRequestBody(modifiedBody)
 					}
 					proxywasm.ResumeHttpRequest()
